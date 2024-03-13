9a2dcd05c0d3b7c69945b2982b2707d06f95cc65
aleksei <aleksei.podmogilnoi@kuehne-nagel.com>
2024-03-13 21:08:25 +0200

    Setup deployment scripts for front-end

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+          TEST master
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

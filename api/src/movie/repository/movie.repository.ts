99159af7517064b5c82b04fe0208697053fe03ee
aleksei <aleksei.podmogilnoi@kuehne-nagel.com>
2024-03-17 15:10:07 +0200

    Add movie layout page. Add search by title. Add filter support on backend. Add Input component

--- api/src/movie/repository/movie.repository.ts
+++ api/src/movie/repository/movie.repository.ts
@@ -1,6 +1,7 @@
 import { MovieEntity } from '../entity/movie.entity';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, Repository, SelectQueryBuilder } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { MovieRequest } from '../types';
 
 @Injectable()
 export class MovieRepository extends Repository<MovieEntity> {
@@ -8,10 +9,26 @@ export class MovieRepository extends Repository<MovieEntity> {
     super(MovieEntity, dataSource.createEntityManager());
   }
 
-  async findAll() {
-    return this.dataSource
+  async findAll(request: MovieRequest) {
+    const query = this.dataSource
       .getRepository(MovieEntity)
-      .createQueryBuilder('movie')
-      .getMany();
+      .createQueryBuilder('movie');
+
+    this.applyFilters(query, request);
+    return query.getMany();
+  }
+
+  private applyFilters(
+    query: SelectQueryBuilder<MovieEntity>,
+    request: MovieRequest,
+  ) {
+    const { alias } = query;
+    const { title } = request;
+
+    if (title) {
+      query.orWhere(`LOWER(${alias}.title) LIKE :title`, {
+        title: `%${title.toLowerCase()}%`,
+      });
+    }
   }
 }
